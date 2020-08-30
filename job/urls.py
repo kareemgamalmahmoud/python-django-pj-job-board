90798bd2432442f68c34254b3215d37571bf8620
kareemgamalmahmoud <kareem.shehata1999@gmail.com>
2020-08-30 18:29:36 +0200

    first commit

---
+++ job/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path , include
+from . import views
+
+app_name='job'
+
+urlpatterns = [
+    path('' , views.job_list , name='job_list'),
+    path('add', views.add_job, name='job_detail'),
+    path('<str:slug>' , views.job_detail , name='job_detail'),
+
+]
